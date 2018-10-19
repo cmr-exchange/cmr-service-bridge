0b5c179dd373439d4248f7dd5dce6cab418964f0
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-10-19 12:37:47 -0500

    CMR-5192: Added more system tests.

--- test/cmr/opendap/tests/system/app/sizing/core.clj
+++ test/cmr/opendap/tests/system/app/sizing/core.clj
@@ -20,7 +20,7 @@
         variable-id "V1200267322-HMR_TME"
         options (-> {}
                     (request/add-token-header (util/get-sit-token)))]
-    (testing "GET without bounding box ..."
+    (testing "With gran and var ..."
       (let [response @(httpc/get
                        (format (str "http://localhost:%s"
                                     "/service-bridge/size-estimate/collection/%s"
@@ -34,5 +34,37 @@
         (is (= 200 (:status response)))
         (is (= "cmr-service-bridge.v2.1; format=json"
                (get-in response [:headers :cmr-media-type])))
-        (is (= [{:bytes 6220800, :gb 0.005793571472167969, :mb 5.9326171875}]
+        (is (= [{:bytes 6220800
+                 :gb 0.005793571472167969
+                 :mb 5.9326171875}]
+               (util/parse-response response)))))
+    (testing "With gran only, implicitly all variables ..."
+      (let [response @(httpc/get
+                       (format (str "http://localhost:%s"
+                                    "/service-bridge/size-estimate/collection/%s"
+                                    "?granules=%s")
+                               (test-system/http-port)
+                               collection-id
+                               granule-id)
+                       options)]
+        (is (= 200 (:status response)))
+        (is (= "cmr-service-bridge.v2.1; format=json"
+               (get-in response [:headers :cmr-media-type])))
+        (is (= [{:bytes 6220800
+                 :gb 0.005793571472167969
+                 :mb 5.9326171875}]
+               (util/parse-response response)))))
+    (testing "With no gran or variables; implicitly all collection granules and variables ..."
+      (let [response @(httpc/get
+                       (format (str "http://localhost:%s"
+                                    "/service-bridge/size-estimate/collection/%s")
+                               (test-system/http-port)
+                               collection-id)
+                       options)]
+        (is (= 200 (:status response)))
+        (is (= "cmr-service-bridge.v2.1; format=json"
+               (get-in response [:headers :cmr-media-type])))
+        (is (= [{:bytes 6220800
+                 :gb 0.005793571472167969
+                 :mb 5.9326171875}]
                (util/parse-response response)))))))
